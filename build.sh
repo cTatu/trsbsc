754df74fb7e6a582b374577211e61e51cc5b2d40
Cristian Tatu <cristian.cat.tatu@gmail.com>
2025-05-21 12:15:20 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ wget https://storage.googleapis.com/dart-archive/channels/stable/release/latest/
 ar x dart_3.8.0-1_amd64.deb
 mkdir dart-sdk
 tar -xf data.tar.xz -C dart-sdk/
-export PATH=$(pwd)/dart-sdk/usr/lib/dart/bin/:$PATH
+export PATH=$(pwd)/dart-sdk/usr/lib/dart/bin/:$HOME/.pub-cache/bin:$PATH
 
 dart pub global activate jaspr_cli
 jaspr build
